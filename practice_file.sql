b7fb5838d7d7f05ddd21d883589d76697c5dc755
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-01 22:35:43 +0530

    Update practice_file.sql

--- practice_file.sql
+++ practice_file.sql
@@ -17,7 +17,24 @@ insert into Employee values(
 'SHGIVAKUMAR', '20231CSE' , 25000, 'A+' , 25);
     insert into Employee values('VIKAS', '20231ISE' , 15000, 'B+' , 15);
 
-select avg(gross)
-from Employee where grade ='B+' or grade = 'C+';
+-----------------------------------------
+2. min :- to Find the Minimum Value
+Display the minimum  value from table
+-----------------------------------------
 
+drop table if exists DEPT030;
+create table Employee(
+   Emp_name varchar(10) primary key,
+   Emp_Roll varchar(10) not null ,
+   sal int ,
+    grade  varchar(10),
+    gross varchar(10)
+    );
+
+insert into Employee values(
+'SHGIVAKUMAR', '20231CSE' , 25000, 'A+' , 25);
+    insert into Employee values('VIKAS', '20231ISE' , 15000, 'B+' , 15);
+
+select min(gross)
+from Employee where grade ='B+' or grade = 'C+';
 
