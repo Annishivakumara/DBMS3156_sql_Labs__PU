23da493afc6a8da5246f6e5a4f8ef14e20fdd41c
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-01 22:40:47 +0530

    Update practice_file.sql

--- practice_file.sql
+++ practice_file.sql
@@ -96,3 +96,32 @@ insert into Employee values(
 
 select count(*)
 from Employee where grade ='B+' or grade = 'C+';
+
+
+COMPLETE CODE OF THE ABOVE AGRGATION 
+drop table if exists  Employee;
+create table Employee(
+   Emp_name varchar(10) primary key,
+   Emp_Roll varchar(10) not null ,
+   sal int ,
+    grade  varchar(10),
+    gross varchar(10)
+    );
+
+    insert into Employee values('SHIVAKUMAR', '20231CSE' , 25000, 'A+' , 25);
+    insert into Employee values('VIKAS', '20231ISE' , 15000, 'B+' , 15);
+    insert into Employee values('CHETHAN', '20231ECE' , 10000, 'C+' , 10);
+    insert into Employee values('SHREYAS', '20231MECH' , 5000, 'D+' , 5);
+
+
+select distinct *
+from Employee where grade ='B+' or grade = 'C+' or grade = 'D+' or gross= 25
+--order by gross desc
+group by Emp_Roll and Emp_name;
+--new table adding to existing table
+alter table Employee
+ add  City varchar(20);
+update Employee set City = 'BENGALURU'  where  gross=25 or gross=15 or gross=10 or gross = 5;
+-- renameing the gross  to the year salary
+alter table Employee
+ rename column sal to Year_salary;
