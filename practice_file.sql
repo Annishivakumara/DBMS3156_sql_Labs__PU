cefa7a2b04f14d96c86ecf51d9c9fee0794e50de
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-01 22:39:24 +0530

    Update practice_file.sql

--- practice_file.sql
+++ practice_file.sql
@@ -77,4 +77,22 @@ insert into Employee values(
 select max(gross)
 from Employee where grade ='B+' or grade = 'C+';
 
+---------------------------------------------
+5. Max :- to Find the Maximum Value
+Display the Maximum  value from table
+------------------------------------------
+drop table if exists DEPT030;
+create table Employee(
+   Emp_name varchar(10) primary key,
+   Emp_Roll varchar(10) not null ,
+   sal int ,
+    grade  varchar(10),
+    gross varchar(10)
+    );
 
+insert into Employee values(
+'SHGIVAKUMAR', '20231CSE' , 25000, 'A+' , 25);
+    insert into Employee values('VIKAS', '20231ISE' , 15000, 'B+' , 15);
+
+select count(*)
+from Employee where grade ='B+' or grade = 'C+';
