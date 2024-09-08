b55298ce704ba551b622c0bcc7c6b894e6721943
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-09-08 22:28:27 +0530

    Update University.sql

--- University.sql
+++ University.sql
@@ -1,47 +1,57 @@
-Drop database if exists Student;
---creating database 
-create database Student;
-use Student;
+drop database exists University;
+create database University;
+use University;
 
-create table Student (
- Sid int primary key, Sname varchar(20), Major varchar(20) , level varchar(20),  DOB date );
-  
-create table faculty(
- Fid int  primary key, Fname varchar(20) , Deport int);
- 
- create table Course(
- Cname varchar(20) primary key , Meets varchar(20) , Room varchar(20) Fid int , foreign key(Fid) references faculty(Fid) , on delete cascade);
- 
- create table Enrolled(
- Sid int , foreign key(Sid) references Student(Sid), Cname varchar(20) ,  foreign key(Cname) references key(Cname) , on delete casecade);
- 
- 
-insert into Student values(
-0619, 'Shivu', 'CSE2001', 'Senior' , 2005-10-17);
-0620, 'Shashi' , 'CSE2002', 'Junior' , 2006-11-20),
-0621, 'Rakshu' , 'CSE2003', 'Senior' , 2005-01-03),
-0622, 'Manasa' , 'Math2005', 'Senior' , 2004-12-03);
-
-insert into  faculty values(
-101, 'Prince' , 'DSA'),
-111, 'Deepthi' , 'DBMS'),
-222, 'Shwetha' , 'FDA'),
-333, 'Pradeep' , 'MATH');
-
-insert into Course values(
- 'DBMS','LFL05','UF01', 101),
-'DSA','LFL06','UF02', 102),
- 'MATH','LFL07','UF03', 103),
- 'FDA','LFL08','UF04', 104),
+create table Student(
+S_id int primary key, Sname varchar(20) , Major varchar(20),
+ Addres varchar(20), level varchar(20), age int , DOB Date 
 );
 
-insert into Enrolled values(
-   2023,'DBMS'),
- 2024,'DSA'),
- 2025,'MATH'),
- 2026,'FDA');
+create table faculty (
+F_id int primary key,
+Fname varchar(20) ,
+Deport  varchar(10));
+
+create table Course (
+Cname varchar(10) primary key,
+Meets varchar(20), 
+Room varchar(20),
+F_id int, 
+foreign key(F_id) references faculty(F_id));
+
+
+create table Enrolled(
+S_id int , Cname varchar(10),
+foreign key (S_id) references Student(S_id),
+foreign key (Cname)  references Course(Cname)
 );
+ 
+insert into Student values
+(2023, 'SHIVU', 'DBMS' , 'Bengaluru', '2nd', 19, '2004-05-17'),
+(2024, 'Shreyas', 'DSA' , 'Bengaluru', '2nd', 20, '2003-05-17'),
+(2025, 'pradeep', 'MATH' , 'Bengaluru', '2nd', 21, '2002-05-15'),
+(2026, 'Preetham', 'FDA' , 'Bengaluru', '2nd', 23, '2000-05-15');
 
 
+insert into faculty values
+(101,'Deepthi', 'DBMS'),
+(102,'Prince', 'DSA'),
+(103,'Pradeep', 'MATH'),
+(104,'Shwetha', 'FDA');
+
+insert into Course values
+( 'DBMS','LFL05','UF01', 101),
+( 'DSA','LFL06','UF02', 102),
+( 'MATH','LFL07','UF03', 103),
+( 'FDA','LFL08','UF04', 104);
+
+insert into Enrolled values
+( 2023,'DBMS'),
+( 2024,'DSA'),
+( 2025,'MATH'),
+( 2026,'FDA');
+
+ 
+ 
 select *
 from University;
