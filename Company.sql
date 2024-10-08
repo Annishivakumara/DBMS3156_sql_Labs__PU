3e38ba341e3e31e4f6757d486fb77685578a90e6
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-08 22:11:02 +0530

    Update Company.sql

--- Company.sql
+++ Company.sql
@@ -8,13 +8,35 @@ select Bdate , Address
 +------------+--------------------------+
 
 --2. Select all Employee SSn
-SELECT  ALL Ssn
-from Employee;
-
+ select all Ssn
+    -> from employee;
++-----------+
+| Ssn       |
++-----------+
+| 123456789 |
+| 333445555 |
+| 453453453 |
+| 666884444 |
+| 888665555 |
+| 987654321 |
+| 987987987 |
+| 999887777 |
++-----------+
 --3.Retrive the combination of employee ssn and deportment dname in the database
-select Ssn , Dname
-from Department , Employee where Dnumber=Dno;
-
+ select Ssn, Fname,Dname
+    -> from employee , department where Dnumber=Dno;
++-----------+----------+----------------+
+| Ssn       | Fname    | Dname          |
++-----------+----------+----------------+
+| 123456789 | John     | Research       |
+| 333445555 | Franklin | Research       |
+| 453453453 | Joyce    | Research       |
+| 666884444 | Ramesh   | Research       |
+| 888665555 | James    | Headquarters   |
+| 987654321 | Jennifer | Administration |
+| 987987987 | Ahmad    | Administration |
+| 999887777 | Alicia   | Administration |
++-----------+----------+----------------+
 --4. Retrive the name and address of Employee who work  for the Research deportment 
 select  Fname , Address
 from Employee  as E , Department as D where   D.Dnumber=E.Dno and Dname="Research";
