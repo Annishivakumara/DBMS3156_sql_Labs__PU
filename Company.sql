1ac684c6464441e88759031e549f49ef559f6cb2
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-17 23:05:05 +0530

    Update Company.sql

--- Company.sql
+++ Company.sql
@@ -23,6 +23,7 @@
 | 987987987 |
 | 999887777 |
 +-----------+
+       
 --3.Retrive the combination of employee ssn and deportment dname in the database
  select Ssn, Fname,Dname
  from employee , department where Dnumber=Dno;
@@ -38,6 +39,7 @@
 | 987987987 | Ahmad    | Administration |
 | 999887777 | Alicia   | Administration |
 +-----------+----------+----------------+
+       
 --4. Retrive the name and address of Employee who work  for the Research deportment 
  select Fname , Address
  from employee as E , department as D where D.Dnumber=E.Dno and Dname="Research";
@@ -49,6 +51,7 @@
 | Joyce    | 5631 Rice, Houston, TX   |
 | Ramesh   | 975 Fire Oak, Humble, TX |
 +----------+--------------------------+
+       
 --5.Lwngth Quastion You should Understand By using Solution of the code
 SELECT Pnumber, Lname, Address, Bdate Mgr_ssn
 from Department  as D, Employee as E , Project  as P where
@@ -63,6 +66,7 @@ D.Dnumber=P.Dnum and E.Ssn=D.Mgr_ssn and Plocation="Stafford";
 |      30 | Wallace | Jennifer | 291 Berry, Bellaire, TX | 1941-06-20 | 987654321 |
 |      30 | Wong    | Franklin | 638 Voss, Houston, TX   | 1955-12-08 | 333445555 |
 +---------+---------+----------+-------------------------+------------+-----------+
+       
        
 --6. Retrive All Employee's Whose Address is in Houston Taxes
 SELECT  ALL  *
