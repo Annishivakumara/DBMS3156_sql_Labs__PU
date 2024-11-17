5ac8733a363cf771de38226cbb3c188b314c54da
Anni <142305502+Annishivakumara@users.noreply.github.com>
2024-11-17 23:14:19 +0530

    Update salesman.sql

--- salesman.sql
+++ salesman.sql
@@ -45,3 +45,19 @@ INSERT INTO ORDERS (Ord_no, Purchase_Amt, Ord_Date, Customer_id, Salesman_id) VA
 (03, 80000, '2024-08-21', 7090, 2024004),
 (04, 80000, '2024-08-21', 7090, 2024004),
 (05, 180000, '2024-09-17', 9036, 2024005);
+
+
+
+
+--Queries
+--Q1: Q:Count the customers with grades above Bangalore average.
+select grade,count(distinct C_id)from customer group by grade having grade>(select avg(grade) from customer where C_city="Bangalore");
+
+--Q2: find the name and number of all salesman who had more than one customer
+select salesman_id , S_name from salesman_s s where 1<(select count(*)from customer where salesman_id=s.salesman_id);
+
+--Q3: 
+select s.salesman_id,S_name,C_name, commission from salesman_s s,customer c where s.S_city=c.C_city union select salesman_id,S_name,'No match',commission from salesman_s where not S_city=any (select C_city from customer) order by 2 desc;
+
+--Q4: 
+create view ElitsSalesman as select b.ord_date, a.salesman_id,a.S_name from salesman_s a, orders b where a.salesman_id=b.salesman_id and b.purchase_amt =(select max(purchase_amt) from orders c where c.ord_date=b.ord_date);
